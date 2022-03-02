d58a00f8378dee226cf9a32630b92a84e56b6aae
GitHub Action <action@github.com>
2022-03-02 19:45:33 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/tognitos/intellijstringconcatheredocplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.tognitos.intellijstringconcatheredocplugin.services
+
+import com.github.tognitos.intellijstringconcatheredocplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
