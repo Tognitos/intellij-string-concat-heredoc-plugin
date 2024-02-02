97b645fbb7b61150d9ad180bc0d6aab21158188c
Matteo Togni <matteo.togni@holidaycheck.com>
2024-02-02 22:05:27 +0100

    test-docs: Join a doc comment

--- src/test/kotlin/com/github/tognitos/intellijstringconcatheredocplugin/TestCompletion.kt
+++ src/test/kotlin/com/github/tognitos/intellijstringconcatheredocplugin/TestCompletion.kt
@@ -3,9 +3,7 @@
  * @see https://github.com/JetBrains/intellij-community/blob/idea/233.14015.106/spellchecker/testSrc/com/intellij/spellchecker/inspector/AcceptWordAsCorrectTest.java
  * @see https://github.com/JetBrains/intellij-community/blob/idea/233.14015.106/java/java-tests/testSrc/com/intellij/copyright/JavaCopyrightTest.kt
  * @see https://github.com/JetBrains/intellij-community/blob/idea/233.14015.106/platform/testFramework/src/com/intellij/testFramework/fixtures/CodeInsightTestFixture.java
- */
-
-/**
+ *
  * An actual real life example `ComparingStringReferencesInspectionTest`
  * @see https://github.com/JetBrains/intellij-sdk-code-samples/blob/main/comparing_string_references_inspection/src/test/java/org/intellij/sdk/codeInspection/ComparingStringReferencesInspectionTest.java
  */
